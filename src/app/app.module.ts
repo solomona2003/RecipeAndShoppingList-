84ec71ba774a1581c72dbb4ced1798e12c2cac99
Solomon <solomona2003@yahoo.com>
2017-08-22 13:37:15 +0200

    added feature modules for auth, shopping list, and recipe, each with own route module, also a shared module for the dropdown directive imported into recipe module and app module. in app module too  since header is loaded as a  into app.component.ts with a selector  as opposed to with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,57 +1,39 @@
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { AuthModule } from './auth/auth.module';
+import { SharedModule } from './shared/shared.module';  // for header which is in app.component
+import { RecipesModule } from './recipes/recipes.module';
 import { AuthGuard } from './auth/auth-guard.service';
 import { AuthService } from './auth/auth.service';
 import { DataStorageService } from './shared/service/data-storage.service';
 import { RecipeService } from './recipes/service/recipe.service';
 import { ShoppingListService } from './shopping-list/service/shopping-list.service';
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { AppRouteModule } from './routing/app-route.module';
+import { HttpModule } from '@angular/http';
 
-import { FormsModule, ReactiveFormsModule} from '@angular/forms';
- 
-import {HeaderComponent} from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
-import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
-import { RecipesItemComponent } from './recipes/recipes-list/recipes-item/recipes-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { DropdownDirective } from './shared/directive/dropdown.directive';
-import {AppRouteModule} from './routing/app-route.module';
-import { RecipesLandComponent } from './recipes/recipes-land/recipes-land.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { HttpModule} from '@angular/http';
-import { SignInComponent } from './auth/sign-in/sign-in.component';
-import { SignUpComponent } from './auth/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipesListComponent,
-    RecipesDetailComponent,
-    RecipesItemComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
-    DropdownDirective,
-    RecipesLandComponent,
-    RecipeEditComponent,
-    SignInComponent,
-    SignUpComponent,
-
-
   ],
+
   imports: [
     BrowserModule,
     FormsModule,
     AppRouteModule,
-    ReactiveFormsModule,
-    HttpModule
+    HttpModule,
+    RecipesModule,
+    AuthModule,
+    SharedModule,
+    ShoppingListModule
   ],
-  providers: [ ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
+  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/app/recipes/recipes-routing.module.ts
@@ -0,0 +1,38 @@
+import { RecipesDetailComponent } from './recipes-detail/recipes-detail.component';
+import { AuthGuard } from './../auth/auth-guard.service';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipesLandComponent } from './recipes-land/recipes-land.component';
+import { RecipesComponent } from './recipes.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+// routes not router
+const recipesRoutes: Routes = [
+
+    // recipes
+    // { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+
+    {
+        path: 'recipes', component: RecipesComponent, children: [
+            { path: '', component: RecipesLandComponent },
+            { path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard] }, // for add new Recipe button
+            { path: ':id', component: RecipesDetailComponent },
+            // for editing existing recipe on edit recipe button on recipe details
+            { path: ':id/edit', component: RecipeEditComponent, canActivate: [AuthGuard] }
+        ]
+    },
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forChild(recipesRoutes)
+    ],
+
+    exports: [
+        RouterModule
+
+    ]
+})
+export class RecipesRoutingMudule {
+
+}
